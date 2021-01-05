9d698224423fcc56edc332eb4cda64a91e280619
alexdivadi <mrbarkface513@gmail.com>
2021-01-04 17:33:32 -0800

    Upload on index

--- ClimateAI/app.py
+++ ClimateAI/app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from werkzeug.utils import secure_filename
 from model import CNN_Model
 
+UPLOAD_FOLDER = 'static/uploads/'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 model = CNN_Model()
 
 @app.route("/", methods=["GET"])
 def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+	    flash('No file part')
+            return redirect(request.url)
+        f = request.files['filename']
+        extension = f.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+        if not extension:
+            flash("File must be image!")
+            return redirect(request.url)
+        try:
+            pred = model.predict_anomaly(f)
+            flash("Predicted class: %s" % pred)
+        except:
+            flash("File must be 40x24 pixels")
+            return redirect(request.url)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template("index.html", filename=filename))
     return render_template("index.html")
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 @app.route("/result/<prediction>")
 def result(prediction):
